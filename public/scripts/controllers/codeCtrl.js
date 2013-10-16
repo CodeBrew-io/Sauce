fcfc7d60e54bfccca18d9248df55ae7799b0be63
manbear <eric.comte.marois@gmail.com>
2013-10-16 00:35:07 -0400

    I've made the horizontal scrollbar of each codemirror independant.  (Sprint5: [UI] Remove horizontal sync)

--- public/scripts/controllers/codeCtrl.js
+++ public/scripts/controllers/codeCtrl.js
@@ -32,7 +32,14 @@ app.controller('CodeCtrl', function CodeCtrl($scope, $timeout, snippets, scalado
     $scope.code = "";
     $scope.insightCode = "";
 
+    /* Defining the Left and Right CodeMirroir */
+    $scope.cmLeft = null;
+    $scope.cmRight = null;
+
+    $scope.insightShow = true;
+
     $scope.options = {
+      fixedGutter: false,
       lineNumbers: true,
       mode: 'text/x-scala',
       theme: 'solarized dark',
@@ -55,14 +62,41 @@ app.controller('CodeCtrl', function CodeCtrl($scope, $timeout, snippets, scalado
 
         // }, 1000);
 
-        $scope.insightCode = insight($scope.code);
+        //$scope.insightCode = insight($scope.code);
+      },
+      onScroll: function(cm) {
+        if ($scope.cmLeft === null) {
+          $scope.cmLeft = cm;
+        }
+
+        var scrollLeftInfo = cm.getScrollInfo();
+        if ($scope.cmRight !== null) {
+          $scope.cmRight.scrollTo(null, scrollLeftInfo['top']);
+        }
+      },
+      onLoad: function(cm) {
+        $scope.cmLeft = cm;
       }
     };
     $scope.options2 = {
+      fixedGutter: false,
       lineNumbers: true,
       mode: 'text/x-scala',
       theme: 'solarized light',
-      readOnly: 'nocursor'
+      readOnly: 'nocursor',
+      onScroll: function(cm) {
+        if($scope.cmRight === null) {
+          $scope.cmRight = cm;
+        }
+
+        var scrollRightInfo = cm.getScrollInfo();
+        if ($scope.cmLeft !== null) {
+          $scope.cmLeft.scrollTo(null, scrollRightInfo['top']);
+        }
+      },
+      onLoad: function(cm) {
+        $scope.cmRight = cm;
+      }
     };
 
     $scope.options3 = {
@@ -71,10 +105,24 @@ app.controller('CodeCtrl', function CodeCtrl($scope, $timeout, snippets, scalado
       readOnly: 'nocursor'
     };
   })();
+  
+  (function() { /* Insight toggling */
+    $scope.insightToggler = function() {
+        $scope.insightShow = !$scope.insightShow;
+    }
+  })();
 
-  /* This part of the code is for the User's behavior in the header of the site */
-  $scope.userDropDownShow = false;
-  $scope.onUserClick = function() {
-    $scope.userDropDownShow = !$scope.userDropDownShow;
-  }
+  (function() { /* This part of the code is for the User's behavior in the header of the site */
+    $scope.userDropDownShow = false;
+    $scope.onUserClick = function() {
+      $scope.userDropDownShow = !$scope.userDropDownShow;
+    }
+  })();
+
+  (function() { /* The showing of the Modal */
+    $scope.modalShow = true;
+    $scope.showSettingsModal = function() {
+      $scope.modalShow = !$scope.modalShow;
+    }
+  })();
 });
