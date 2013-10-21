d129f0621c3de59e31880d33bf1a598ec56c3463
Guillaume Massé <masgui@gmail.com>
2013-10-21 15:13:25 -0400

    move root project to web/ + remove plugin overriding settings (in my resolver)

--- project/Settings.scala
+++ project/Settings.scala
@@ -4,32 +4,30 @@ import Keys._
 import com.typesafe.sbt.SbtGit._
 
 object Settings {
-	val default = Seq(
+	val default = Project.defaultSettings ++ Seq(
 		scalaVersion := "2.10.3",
 		organization := "ca.polymtl.log4900"
 	) ++ versionWithGit
 
 	// no play
-	import play.Keys._
-    val noplay = Seq(
-    	// no typescript
-		requireJs := Seq(),
-		requireJsFolder := "",
-		requireJsShim := "",
-		requireNativePath := None,
+	// import play.Keys._
+ //    val noplay = Seq(
+ //    	// no typescript
+	// 	requireJs := Seq(),
+	// 	requireJsFolder := "",
+	// 	requireJsShim := "",
+	// 	requireNativePath := None,
 
-		// no livereload
-		playAssetsDirectories := Seq() 
-    )
+	// 	// no livereload
+	// 	playAssetsDirectories := Seq() 
+ //    )
 
 	// scrooge
 	import com.twitter.scrooge._
 	import ScroogeSBT._
 	lazy val scrooge = 
-		Project.defaultSettings ++ 
 		Settings.default ++ 
 		ScroogeSBT.newSettings ++ 
-		noplay ++
 		Seq(
     		scroogeBuildOptions := Seq("--ostrich","--finagle"),
     		libraryDependencies ++= Dependencies.scroogeStack
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -10,9 +10,9 @@ addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.2.0")
 
 addSbtPlugin("com.twitter" %% "scrooge-sbt-plugin" % "3.9.0")
 
-addSbtPlugin("com.jamesward" %% "play-auto-refresh" % "0.0.5")
+addSbtPlugin("com.jamesward" %% "play-auto-refresh" % "0.0.6")
 
-addSbtPlugin("com.github.mumoshu" % "play2-typescript" % "0.2-RC10")
+addSbtPlugin("com.github.mumoshu" % "play2-typescript" % "0.2-RC11")
 
 addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "0.6.2")
 
