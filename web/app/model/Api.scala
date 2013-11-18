c5e0bb51267f5b78b525bd47d282c095b817349b
Guillaume Massé <masgui@gmail.com>
2013-11-18 13:48:52 -0500

    consistent json structure for insight result + test

--- web/app/model/Api.scala
+++ web/app/model/Api.scala
@@ -5,71 +5,33 @@ import play.api.libs.functional.syntax._
 
 import com.twitter.util.Future
 
-import io.codebrew.api._
+import io.codebrew.api.eval._
 
 object EvalService {
+	import Api._
+
 	def apply(json: JsValue): Future[JsValue] = {
-		Api.all(json, ping _, insight _, autocomplete _, fallback _)
+		all(json, insight _, autocomplete _, fallback _)
 	}
 
-	private val callback = "callback_id"
-	private def unavailable(serviceName: String, cid: Int): Future[JsValue] = Future(
-		JsObject(Seq(
-			"insight" -> JsString(s"$serviceName service unavailable"),
-			callback -> JsNumber(cid)
-		))
-	)
-	private def ping(cid: Int): Future[JsValue] = Future {
-		JsObject(Seq(callback -> JsNumber(cid)))
-	}
 	private def insight(code: String, cid: Int): Future[JsValue] = {
 		Registry.getEval.map(service => {
-			service.insight(code).map(result => {
-
-				val insightResult = result.insight.map( r => Seq(
-					"insight" -> JsString(r.insight),
-					"output" -> JsString(r.output)
-				)).getOrElse(Seq())
-
-				val compilationInfo = 
-					if(result.infos.isEmpty) Seq()
-					else Seq(
-						"compilationInfo" -> JsArray(result.infos.map(c => Json.obj(
-							"message" ->  JsString(c.message),
-							"pos" ->  JsNumber(c.pos),
-							"severity" -> JsNumber(c.severity.value)
-						)))
-					)
-				JsObject(insightResult ++ compilationInfo ++ Seq(callback -> JsNumber(cid)))
-			})
+			service.insight(code).map(i => insightResult(i, cid))
 		}).getOrElse(unavailable("insight", cid))
 	}
 	private def autocomplete(code: String, position: Int, cid: Int): Future[JsValue] = {
 		Registry.getEval.map(service => {
-			service.autocomplete(code, position).map(completions => {
-				JsObject(Seq(
-					"completions" -> JsArray(completions.map(c => Json.obj(
-            			"name" -> c.name,
-            			"signature" -> c.signature
-          			))),
-					callback -> JsNumber(cid)
-				))
-			})
+			service.autocomplete(code, position).map(a => autocompleteResult(a, cid))
 		}).getOrElse(unavailable("autocomplete", cid))
 	}
 	private def fallback(): Future[JsValue] = Future {
-		JsObject(Seq(
-			"error" -> JsString("invalid request")
-		))
+		JsObject(Seq("error" -> JsString("invalid request")))
 	}
 }
 
 object Api {
 	val callback = (__ \ "callback_id").read[Int]
-	val ping: Reads[Int] = (__ \ "ping").read(
-		callback
-	)
-
+	
 	val autocomplete: Reads[(String,Int,Int)] =
 		(__ \ "autocomplete").read(
 			(__ \ "code").read[String] and
@@ -86,21 +48,56 @@ object Api {
 
 	def all[T](
 		json: JsValue,
-		pingF: (Int) => T, 
 		insightF: (String,Int) => T,
 		autocompleteF: (String, Int, Int) => T,
 		fallback: () => T 
 	): T = {
 
-		ping.reads(json) match {
-			case JsSuccess(cid,_) => pingF(cid)
-			case _:JsError => insight.reads(json) match {
-				case JsSuccess((code, cid),_) => insightF(code, cid)
-				case _:JsError => autocomplete.reads(json) match {
-					case JsSuccess((code, pos, cid),_) => autocompleteF(code, pos, cid)
-					case _:JsError => fallback()
-				}
+		insight.reads(json) match {
+			case JsSuccess((code, cid),_) => insightF(code, cid)
+			case _:JsError => autocomplete.reads(json) match {
+				case JsSuccess((code, pos, cid),_) => autocompleteF(code, pos, cid)
+				case _:JsError => fallback()
 			}
 		}
 	}
+
+	private val callback_id = "callback_id"
+	def insightResult(r: Result, cid: Int): JsValue = {
+		val (insight, output) = r.insight.map(i => (i.insight, i.output)).getOrElse(("",""))
+		val groupedInfos = r.infos.groupBy(_.severity).map{ case (t, sevs) =>
+			s"${t.toString.toLowerCase}s" -> JsArray(sevs.map(s => JsObject(Seq(
+				"message" -> JsString(s.message),
+				"position" -> JsNumber(s.pos)
+			))))
+		}.to[Seq]
+
+		val infos = 
+			if(groupedInfos.isEmpty) Seq("errors" -> JsArray(), "warnings" -> JsArray(), "infos" -> JsArray())
+			else groupedInfos
+
+		JsObject(
+			infos ++
+			Seq(
+				"insight" -> JsString(insight),
+				"output" -> JsString(output),
+				callback_id -> JsNumber(cid)
+			)
+		)
+	}
+	def autocompleteResult(completions: Seq[Completion], cid: Int): JsValue = {
+		JsObject(Seq(
+			"completions" -> JsArray(completions.map(c => JsObject(Seq(
+				"name" -> JsString(c.name),
+				"signature" -> JsString(c.signature)
+			)))),
+			callback_id -> JsNumber(cid)
+		))
+	}
+	def unavailable(serviceName: String, cid: Int): Future[JsValue] = Future(
+		JsObject(Seq(
+			"insight" -> JsString(s"$serviceName service unavailable"),
+			callback_id -> JsNumber(cid)
+		))
+	)
 }
